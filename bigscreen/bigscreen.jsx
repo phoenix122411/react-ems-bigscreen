07eef9a4755f77992b0d2c2da78173bedae94056
unknown <>
2016-09-09 16:58:14 +0800

    zmm-bigscreen

--- bigscreen/bigscreen.jsx
+++ bigscreen/bigscreen.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 
 import EchartsComponent from './EchartsComponent.jsx';
 
-
-
+ReactDOM.render(<EchartsComponent name="gauge_YBP" />, document.getElementById('gauge_YBP'));
 for(var i=0; i<chartNameList.length; i++) {
 	ReactDOM.render(<EchartsComponent name={chartNameList[i][0]} />, document.getElementById(chartNameList[i][1]));
 }
