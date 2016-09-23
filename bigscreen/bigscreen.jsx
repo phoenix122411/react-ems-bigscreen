e3031139dcfa4ad23c77953c18e8e13287add81f
unknown <>
2016-09-23 11:17:53 +0800

    zmm-exception

--- bigscreen/bigscreen.jsx
+++ bigscreen/bigscreen.jsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import EchartsComponent from './EchartsComponent.jsx';
 
 ReactDOM.render(<EchartsComponent name="gauge_YBP" />, document.getElementById('gauge_YBP'));
-for(var i=0; i<chartNameList.length; i++) {
-	ReactDOM.render(<EchartsComponent name={chartNameList[i].chart_path} />, document.getElementById(chartNameList[i].target_name));
+if(typeof(chartNameList) != 'undefined') {
+	for(var i=0; i<chartNameList.length; i++) {
+		if(document.getElementById(chartNameList[i].target_name) != null) {
+			ReactDOM.render(<EchartsComponent name={chartNameList[i].chart_path} />, document.getElementById(chartNameList[i].target_name));
+		}
+	}
 }
